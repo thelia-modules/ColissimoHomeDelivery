56569aef4cd59b11b9e24659bcd6797ed569e2d7
Lurivar <lurivar@users.noreply.github.com>
2020-08-04 15:07:16 +0200

    wip

--- EventListeners/APIListener.php
+++ EventListeners/APIListener.php
@@ -8,6 +8,7 @@ use ColissimoHomeDelivery\ColissimoHomeDelivery;
 use OpenApi\Events\DeliveryModuleOptionEvent;
 use OpenApi\Events\OpenApiEvents;
 use OpenApi\Model\Api\DeliveryModuleOption;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Core\Translation\Translator;
 use Thelia\Model\CountryArea;
@@ -15,6 +16,13 @@ use Thelia\Module\Exception\DeliveryException;
 
 class APIListener implements EventSubscriberInterface
 {
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function getDeliveryModuleOptions(DeliveryModuleOptionEvent $deliveryModuleOptionEvent)
     {
         if ($deliveryModuleOptionEvent->getModule()->getId() !== ColissimoHomeDelivery::getModuleId()) {
@@ -55,18 +63,21 @@ class APIListener implements EventSubscriberInterface
         $minimumDeliveryDate = ''; // TODO (calculate delivery date from day of order)
         $maximumDeliveryDate = ''; // TODO (calculate delivery date from day of order
 
-        $deliveryModuleOptionEvent->setDeliveryModuleOptions(
-            (new DeliveryModuleOption())
-                ->setCode('ColissimoHomeDelivery')
-                ->setValid($isValid)
-                ->setTitle('Colissimo Home Delivery')
-                ->setImage('')
-                ->setMinimumDeliveryDate($minimumDeliveryDate)
-                ->setMaximumDeliveryDate($maximumDeliveryDate)
-                ->setPostage($postage)
-                ->setPostageTax($postageTax)
-                ->setPostageUntaxed($postage - $postageTax)
-        );
+        /** @var DeliveryModuleOption $deliveryModuleOption */
+        $deliveryModuleOption = ($this->container->get('open_api.model.factory'))->buildModel('DeliveryModuleOption');
+        $deliveryModuleOption
+            ->setCode('ColissimoHomeDelivery')
+            ->setValid($isValid)
+            ->setTitle('Colissimo Home Delivery')
+            ->setImage('')
+            ->setMinimumDeliveryDate($minimumDeliveryDate)
+            ->setMaximumDeliveryDate($maximumDeliveryDate)
+            ->setPostage($postage)
+            ->setPostageTax($postageTax)
+            ->setPostageUntaxed($postage - $postageTax)
+        ;
+
+        $deliveryModuleOptionEvent->appendDeliveryModuleOptions($deliveryModuleOption);
     }
 
     public static function getSubscribedEvents()
