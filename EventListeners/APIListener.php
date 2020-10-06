2266df7faa8a9434c3cf3e230cf9f1f80bda5180
Nicolas Barbey <223106@supinfo.com>
2020-10-06 12:04:17 +0200

    Add OpenApi compatibility

---
+++ EventListeners/APIListener.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace ColissimoHomeDelivery\EventListeners;
+
+
+use ColissimoHomeDelivery\ColissimoHomeDelivery;
+use OpenApi\Events\DeliveryModuleOptionEvent;
+use OpenApi\Events\OpenApiEvents;
+use OpenApi\Model\Api\DeliveryModuleOption;
+use OpenApi\Model\Api\ModelFactory;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Thelia\Core\Translation\Translator;
+use Thelia\Model\CountryArea;
+use Thelia\Module\Exception\DeliveryException;
+
+class APIListener implements EventSubscriberInterface
+{
+    protected $modelFactory;
+
+    public function __construct(ModelFactory $modelFactory)
+    {
+        $this->modelFactory = $modelFactory;
+    }
+
+    public function getDeliveryModuleOptions(DeliveryModuleOptionEvent $deliveryModuleOptionEvent)
+    {
+        if ($deliveryModuleOptionEvent->getModule()->getId() !== ColissimoHomeDelivery::getModuleId()) {
+            return ;
+        }
+
+        $isValid = true;
+        $postage = null;
+        $postageTax = null;
+
+        try {
+            $module = new ColissimoHomeDelivery();
+            $country = $deliveryModuleOptionEvent->getCountry();
+
+            if (empty($module->getAllAreasForCountry($country))) {
+                throw new DeliveryException(Translator::getInstance()->trans("Your delivery country is not covered by Colissimo"));
+            }
+
+            $countryAreas = $country->getCountryAreas();
+            $areasArray = [];
+
+            /** @var CountryArea $countryArea */
+            foreach ($countryAreas as $countryArea) {
+                $areasArray[] = $countryArea->getAreaId();
+            }
+
+            $postage = $module->getMinPostage(
+                $areasArray,
+                $deliveryModuleOptionEvent->getCart()->getWeight(),
+                $deliveryModuleOptionEvent->getCart()->getTaxedAmount($country)
+            );
+
+            $postageTax = 0; //TODO
+        } catch (\Exception $exception) {
+            $isValid = false;
+        }
+
+        $minimumDeliveryDate = ''; // TODO (calculate delivery date from day of order)
+        $maximumDeliveryDate = ''; // TODO (calculate delivery date from day of order
+
+        /** @var DeliveryModuleOption $deliveryModuleOption */
+        $deliveryModuleOption = $this->modelFactory->buildModel('DeliveryModuleOption');
+        $deliveryModuleOption
+            ->setCode('ColissimoHomeDelivery')
+            ->setValid($isValid)
+            ->setTitle('Colissimo Home Delivery')
+            ->setImage('')
+            ->setMinimumDeliveryDate($minimumDeliveryDate)
+            ->setMaximumDeliveryDate($maximumDeliveryDate)
+            ->setPostage($postage)
+            ->setPostageTax($postageTax)
+            ->setPostageUntaxed($postage - $postageTax)
+        ;
+
+        $deliveryModuleOptionEvent->appendDeliveryModuleOptions($deliveryModuleOption);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        $listenedEvents = [];
+
+        /** Check for old versions of Thelia where the events used by the API didn't exists */
+        if (class_exists(DeliveryModuleOptionEvent::class)) {
+            $listenedEvents[OpenApiEvents::MODULE_DELIVERY_GET_OPTIONS] = array("getDeliveryModuleOptions", 129);
+        }
+
+        return $listenedEvents;
+    }
+}
